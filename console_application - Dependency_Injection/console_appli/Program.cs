551a89765ff7f3af44234148038822e179f86261
Ahmed ghaithan <ghaithanahmed691@gmail.com>
2024-08-08 03:42:20 +0300

    Add files via upload

---
+++ console_application - Dependency_Injection/console_appli/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EF_CORE_Dependency_Injection
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var configuration = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+
+            var Connection_String = configuration.GetSection("constr").Value;
+
+            var services = new ServiceCollection();
+            
+            // adding the services
+            services.AddDbContext<AppDBContext>(options => options.UseSqlServer(Connection_String));
+
+            // calling the services
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            // using the service
+            using (var context = serviceProvider.GetService<AppDBContext>())
+            {
+                foreach (var wallet in context!.Wallets)
+                {
+                    Console.WriteLine(wallet);
+                }
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
