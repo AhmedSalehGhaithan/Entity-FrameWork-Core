22ad65e0956365e7abb95ed76a3daad13fd8d644
Ahmed ghaithan <ghaithanahmed691@gmail.com>
2024-08-08 03:44:31 +0300

    Add files via upload

---
+++ console_application - other configuration/console_appli/AppDBbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CORE_Other_Configuration
+{
+    public class AppDBContext : DbContext
+    {
+        // Represent the collection of all tables
+        public DbSet<Wallets> Wallets { get; set; } = null!;
+        public AppDBContext(DbContextOptions options) : base(options) { }
+
+    }
+}
---
+++ console_application - other configuration/console_appli/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace EF_CORE_Other_Configuration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var configuration = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+
+            var Connection_String = configuration.GetSection("constr").Value;
+
+            var opitionBuilder = new DbContextOptionsBuilder();
+
+            opitionBuilder.UseSqlServer(Connection_String)
+                .LogTo(Console.WriteLine,LogLevel.Information) ;
+
+            var option = opitionBuilder.Options;
+
+            using (var context = new AppDBContext(option))
+            {
+                var w1 = new Wallets { Id = 6, Holder = "Samy", Balance = 1000M };
+                context.Wallets.Add(w1);
+
+                var w2 = new Wallets { Id = 7, Holder = "hany", Balance = 2000M };
+                context.Wallets.Add(w2);
+
+                context.SaveChanges();
+            }
+
+           
+
+
+                Console.ReadKey();
+        }
+    }
+}
