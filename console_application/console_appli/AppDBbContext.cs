3d4f35b15a1e81a3c7892f4a75c596e55d6325b3
Ahmed ghaithan <ghaithanahmed691@gmail.com>
2024-08-08 03:34:57 +0300

    Add files via upload

---
+++ console_application/console_appli/AppDBbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CORE_External_Configuration
+{
+    public class AppDBbContext : DbContext
+    {
+        // Represent the collection of all tables
+        public DbSet<Wallets> Wallets { get; set; } = null!;
+        public AppDBbContext(DbContextOptions options):base(options) { }
+
+
+    }
+}
---
+++ console_application/console_appli/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlTypes;
+
+namespace EF_CORE_External_Configuration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var configration = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+
+            var connection_string = configration.GetSection("constr").Value;
+
+            var optionBuilder = new DbContextOptionsBuilder();
+
+            optionBuilder.UseSqlServer(connection_string);
+
+            var options = optionBuilder.Options;
+
+            using (var context = new AppDBbContext(options))
+            {
+                foreach(var wallet in context.Wallets)
+                {
+                    Console.WriteLine(wallet);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
